6951c1bf6c54b68c76792f12ec94add2e3140e9c
Guilherme Tâmega <guitamega06@gmail.com>
2023-02-13 19:56:33 +0000

    Starting project

---
+++ og-project/components/renderApi/index.tsx
@@ -0,0 +1,28 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+import { sortAndDeduplicateDiagnostics } from "typescript";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const hasTitle = searchParams.has("title");
+    const hasBgColor = searchParams.has("BgColor");
+    const hasColor = searchParams.has("Color");
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "Lorem Ipsum";
+    const BgColor = hasBgColor ? searchParams.get("BgColor") : "lightblue";
+    const Color = hasColor ? searchParams.get("Color") : "black";
+
+    return new ImageResponse((
+        <div style={{backgroundColor:`${BgColor}`}}></div>
+    ))
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, { status: 500 });
+  }
+}
