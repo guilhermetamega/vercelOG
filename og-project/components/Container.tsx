259cd301c5f2bdf10c2725263f5cd824405a04c0
Guilherme Tâmega <guitamega06@gmail.com>
2023-02-24 00:02:19 +0000

    Responsive layout working

--- og-project/components/Container.tsx
+++ og-project/components/Container.tsx
@@ -4,8 +4,13 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+  flex-direction: row;
   flex-wrap: nowrap;
+  scale: 0.8;
+
+  @media only screen and (max-width: 1350px) {
+    flex-direction: column;
+  }
 `;
 
 export default Container;
